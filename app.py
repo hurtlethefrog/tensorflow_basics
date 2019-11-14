ef12d09a131766db93a7bfcc6c15cd6bfed45aa9
Duncan H <duncan.haran@gmail.com>
2019-11-14 14:26:11 -0500

    set up model layers and compile functions

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ import matplotlib.pyplot as plt
 
 fashion_mnist = keras.datasets.fashion_mnist
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
+               'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 data = keras.datasets.fashion_mnist
 
@@ -20,12 +21,26 @@ train_images = train_images / 255.0
 
 test_images = test_images / 255.0
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=25)
+
+test_loss, test_acc =model.evaluate(test_images, test_labels)
+
+print('Tested acc:', test_acc)
